a052f6eb0855bb76b0ae47b90c203d145682a3b4
kristinamiller <kfm287@gmail.com>
2019-09-27 12:32:38 -0700

    project setup

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const db = require("./config/keys").mongoURI;
+
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to mongoDB"))
+  .catch(err => console.log(err));
 
 app.get("/", (req, res) => {
   res.send("Hello App Academy!");
 });
 
-// tell the app to listen on a given port
-// use the port variable if you have one. if not use port 5000.
-
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {console.log(`listening on port ${port}`)})
 
-//nodemon listens for changes in our code.
